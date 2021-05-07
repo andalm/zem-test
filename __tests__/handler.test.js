c2476e37692d6ae21f14cc0b3dfd66db5e056412
Andrés Aldana <a.aldanam@uniandes.edu.co>
2021-05-07 16:25:23 -0500

    Test: remove unuse tests

--- __tests__/handler.test.js
+++
@@ -1,6 +0,0 @@
-const handler = require('../handler')
-
-test('Expected handler statusCode', async () => {
-  let result = await handler.user({event: 'mock-event'})
-  expect(result.statusCode).toBe(200)
-})
