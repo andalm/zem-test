d4e7316979fe945213543e3b9adb35dfbd841719
Andrés Aldana <a.aldanam@uniandes.edu.co>
2021-05-07 00:51:42 -0500

    Refactor: Twitter class accept and object interface to get data (DI)

--- src/common/twitter.js
+++ src/common/twitter.js
@@ -1,14 +1,12 @@
 module.exports = class Twitter {
   
-  async getRecent(limit = 1) {
-    return [{
-      text: 'RT @TwitterDev: 1/ Today we’re sharing our vision for the future of the Twitter API platform!nhttps://t.co/XweGngmxlP',
-      user: {
-        id: 6253282,
-        name: 'Twitter API',
-        description: 'The Real Twitter API. I tweet about API changes, service issues and happily answer questions about Twitter and our API. Don\'t get an answer? It\'s on my website.',
-        url: 'http://t.co/78pYTvWfJd'
-      }
-    }]
+  #source
+  
+  constructor(source) {
+    this.#source = source
+  }
+  
+  async getRecent(count = 1) {
+    return await this.#source.get('statuses/home_timeline', {count})
   }
 }
