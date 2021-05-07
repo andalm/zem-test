8addd8e3cddea343ee703c370de5ab91614fece7
Andrés Aldana <a.aldanam@uniandes.edu.co>
2021-05-07 18:19:00 -0500

    Feat: add user fixture

---
+++ src/adapter/fixtures/user.fixture.js
@@ -0,0 +1,17 @@
+const faker = require('faker')
+
+module.exports = class UserFixture {
+  
+  #data = [{
+    id: '61a7ce5b-7a26-1e8e-9ac8-06474d96a566',
+    firstName: faker.name.firstName(),
+    lastName: faker.name.lastName(),
+    image: faker.image.image(),
+    title: faker.name.title(),
+    text: faker.lorem.text()
+  }]
+  
+  async get(id) {
+    return this.#data.find(user => user.id === id) || {}
+  }
+}
