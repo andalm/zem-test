0f87382ff79d80e3238d8e492d2d2b74f17985ad
Andrés Aldana <a.aldanam@uniandes.edu.co>
2021-05-06 23:10:35 -0500

    Test: refactor the test to describe a bunch of tests

--- src/common/__tests__/twitter.test.js
+++ src/common/__tests__/twitter.test.js
@@ -1,7 +1,9 @@
 const Twitter = require('../twitter')
 
-test('Should receive 5 recent tweets', async () => {
-  const twitter = new Twitter({name: 'mock source'})
-  const recentTweets = await twitter.getRecent(5)
-  expect(recentTweets.length).toBe(5)
+describe('Twitter', () => {
+  it('Should receive 5 recent tweets', async () => {
+    const twitter = new Twitter({name: 'mock source'})
+    const recentTweets = await twitter.getRecent(5)
+    expect(recentTweets.length).toBe(5)
+  })
 })
