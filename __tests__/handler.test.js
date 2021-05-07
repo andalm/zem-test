f4ddfac56bf45ba03b6c12d6d935fe4b4f98dd60
Andrés Aldana <a.aldanam@uniandes.edu.co>
2021-05-06 20:58:24 -0500

    Fix: change wrong test description

--- __tests__/handler.test.js
+++ __tests__/handler.test.js
@@ -1,6 +1,6 @@
 const handler = require('../handler')
 
-test('correct greeting is generated', async () => {
+test('Expected handler statusCode', async () => {
   let result = await handler.user({event: 'mock-event'})
   expect(result.statusCode).toBe(200)
 })
