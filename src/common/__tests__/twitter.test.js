7b0344bc86112b8809b0df8da18278677e3b7dcd
Andrés Aldana <a.aldanam@uniandes.edu.co>
2021-05-06 23:21:30 -0500

    Test: add test for structure validation (red)

--- src/common/__tests__/twitter.test.js
+++ src/common/__tests__/twitter.test.js
@@ -1,9 +1,17 @@
 const Twitter = require('../twitter')
 
 describe('Twitter', () => {
+  const twitter = new Twitter({name: 'mock source'})
+  
   it('Should receive 5 recent tweets', async () => {
-    const twitter = new Twitter({name: 'mock source'})
     const recentTweets = await twitter.getRecent(5)
     expect(recentTweets.length).toBe(5)
+  })
+  
+  it('Should receive 1 recent tweets to validate structure', async () => {
+    const recentTweet = await twitter.getRecent()
+    expect(recentTweet.text).toBeInstanceOf(String)
+    expect(recentTweet.user.description).toBeInstanceOf(String)
+    expect(recentTweet.user.url).toBeInstanceOf(String)
   })
 })
