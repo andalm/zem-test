f8058187b8b300673dd4bbff500ad50e1f2b08cf
Andrés Aldana <a.aldanam@uniandes.edu.co>
2021-05-07 18:20:04 -0500

    Test: refactor user test (green)

--- src/domain/__tests__/user.test.js
+++ src/domain/__tests__/user.test.js
@@ -13,7 +13,22 @@ beforeAll(() => {
 
 describe('User', () => {
   it('Should get a user by id', async () => {
-    const user = await user.get(123456)
-    expect(user).toBeDefined()
+    const userResult = await user.getById('61a7ce5b-7a26-1e8e-9ac8-06474d96a566')
+    expect(userResult).toBeDefined()
+  })
+  
+  it('Should get a user by id to validate structure', async () => {
+    const userResult = await user.getById('61a7ce5b-7a26-1e8e-9ac8-06474d96a566')
+    expect(typeof userResult.id).toBe('string')
+    expect(typeof userResult.firstName).toBe('string')
+    expect(typeof userResult.lastName).toBe('string')
+    expect(typeof userResult.image).toBe('string')
+    expect(typeof userResult.title).toBe('string')
+    expect(typeof userResult.text).toBe('string')
+  })
+  
+  it('Should get an empty object if user doesn`t exist', async () => {
+    const userResult = await user.getById(123156)
+    expect(typeof userResult).toBe('object')
   })
 })
